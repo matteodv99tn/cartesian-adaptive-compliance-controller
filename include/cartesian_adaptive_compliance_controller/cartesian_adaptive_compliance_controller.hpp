eead681f20df018979253f65a8bda99d4f312bff
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2024-02-23 17:26:56 +0100

    Added template of ros2 controller

---
+++ include/cartesian_adaptive_compliance_controller/cartesian_adaptive_compliance_controller.hpp
@@ -0,0 +1,19 @@
+#ifndef CARTESIAN_ADAPTIVE_COMPLIANCE_CONTROLLER_HPP__
+#define CARTESIAN_ADAPTIVE_COMPLIANCE_CONTROLLER_HPP__
+
+
+#include "cartesian_compliance_controller/cartesian_compliance_controller.h"
+#include "qpOASES.hpp"
+
+USING_NAMESPACE_QPOASES
+
+namespace cartesian_adaptive_compliance_controller {
+
+class CartesianAdaptiveComplianceController
+        : public cartesian_compliance_controller::CartesianComplianceController {
+
+        };
+
+}  // namespace cartesian_adaptive_compliance_controller
+
+#endif  // CARTESIAN_ADAPTIVE_COMPLIVECTOR_HPP__

37aa68d0b6319c949f88b8bb8ae12ecddcad74c9
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2024-02-23 17:46:29 +0100

    Implementation notes

--- src/cartesian_adaptive_compliance_controller.cpp
+++ src/cartesian_adaptive_compliance_controller.cpp
@@ -1,1 +1,11 @@
 // Empty
+
+/**
+ * Changes are in the functions:
+ * - on_configure(): this changes might not be relevant to me
+ * - on_activate(): there are changes
+ *      mainly initialisation of publisher/subscribers (for both logging and control purposes)
+ * - update(): this must be changed for sure, since it implements the main control algorithm
+ *      see where we can call the parent class!
+ *
+ */
